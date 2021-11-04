92f4541732d9d4f26b706b754c607e4f10742997
monicasalas86 <monicsalas167@gmail.com>
2021-11-04 00:02:45 -0500

    find model function working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -1,4 +1,14 @@
+const db = require('../../data/db-config')
+
 function find() { // EXERCISE A
+  return db('schemes as sc')
+    .leftJoin('steps as st', 'sc.scheme_id', 'st.scheme_id')
+    .select('sc.*')
+    .count('st.step_id as number_of_steps')
+    .groupBy('sc.scheme_id')
+    .orderBy('sc.scheme_id')
+
+
   /*
     1A- Study the SQL query below running it in SQLite Studio against `data/schemes.db3`.
     What happens if we change from a LEFT join to an INNER join?
