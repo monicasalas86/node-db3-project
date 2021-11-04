8a4d494d5aba8fb03a3f31296eeebfaebccd34d9
monicasalas86 <monicsalas167@gmail.com>
2021-11-04 00:26:50 -0500

    adding post and middleware working

--- api/schemes/scheme-model.js
+++ api/schemes/scheme-model.js
@@ -161,10 +161,12 @@ async function findSteps(scheme_id) { // EXERCISE C
   }
 }
 
-function add(scheme) { // EXERCISE D
+async function add(scheme) { // EXERCISE D
   /*
     1D- This function creates a new scheme and resolves to _the newly created scheme_.
   */
+  const [id] = await db('schemes').insert(scheme)
+  return findById(id)
 }
 
 function addStep(scheme_id, step) { // EXERCISE E
